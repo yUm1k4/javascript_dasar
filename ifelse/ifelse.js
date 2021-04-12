3fc7c470bda4eaea06c12fdf72229c63d249b7e9
yUm1k4 <zaiabdullah99@gmail.com>
2021-04-12 14:15:55 +0700

    6.5 pengkondisian ifelse

---
+++ ifelse/ifelse.js
@@ -0,0 +1,17 @@
+// var angka = prompt('masukkan angka:');
+
+// if (angka % 2 == 0) {
+//     alert(angka + ' adalah bilangan Genap');
+// } else {
+//     alert(angka + ' adalah bilangan Ganjil');
+// }
+
+var angka = prompt('masukkan angka:');
+
+if (angka % 2 == 0) {
+    alert(angka + ' adalah bilangan Genap');
+} else if(angka % 2 == 1) {
+    alert(angka + ' adalah bilangan Ganjil');
+} else {
+    alert('yang adalah masukkan bukan angka.');
+}
