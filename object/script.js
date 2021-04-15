5cd866db88ea033ab4a40d4e77969eabb9d243f5
yUm1k4 <zaiabdullah99@gmail.com>
2021-04-15 13:31:16 +0700

    9.0 object (intro)

---
+++ object/script.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama: "Zainudin",
+    umur: 18,
+    ips: [3, 2.5, 3.2],
+    alamat: {
+        jalan: "Jl. Raya Narogong Km. 12",
+        kota: "Bekasi",
+        provinsi: "Jawa Barat"
+    }
+};
